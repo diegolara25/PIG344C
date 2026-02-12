5dbda08e4a2f5d8c66a41ac69c81ad585f85ee0f
Diego Lara <diegomaulara@gmail.com>
2026-02-11 22:05:51 -0500

    Postaman inicio

--- Mision2/PBpg/energiapi/src/main/java/com/talentotech/energiapi/controller/UserController.java
+++ Mision2/PBpg/energiapi/src/main/java/com/talentotech/energiapi/controller/UserController.java
@@ -5,10 +5,11 @@ import com.talentotech.energiapi.repository.UserRepository;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
 
 
 
@@ -31,6 +32,21 @@ public class UserController {
     public List<User> findAll() {
         
         return userRepository.findAll();
+    }
+
+    //consultar por ID
+    @GetMapping("/{id}")
+    public User findByID(@PathVariable Long id){
+        return userRepository.findById(id).orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND,"Usuario no encontradop"));
+    }
+
+    //modificar
+    @PutMapping("/{id}")
+    public User update(@PathVariable Long id, @RequestBody User userDetails){
+        User user = userRepository.findById(id).orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND,"Usuario no encontradop"));
+        user.setUsername(userDetails.getUsername());
+        user.setEmail(userDetails.getEmail());
+        return userRepository.save(user);
     }
     
     
